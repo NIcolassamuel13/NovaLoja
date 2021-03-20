a3fed5a9b4a00fa8997351e61848e0f3d45206ee
Daniel Castro <d.13castrosilva@gmail.com>
2021-03-20 02:04:54 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -10,8 +10,27 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
 
-
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+           // não colocar no construtur atributos de Icollections não pode 
+        }
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //vai prcorrer a lista de seller somando e usando o função de que já estava pronta 
+            // do seller que já calculava a s venda em um determinado periodo
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0, //pendente
+        Billet = 1, // faturado
+        Canceled = 2 // cancelado
+
+
+
+    }
+}
