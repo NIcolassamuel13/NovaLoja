0113fdb5fde21408c8acc60de6b1adb2a5d05752
Daniel Castro <d.13castrosilva@gmail.com>
2021-03-21 16:59:12 -0300

    seeding service

--- SalesWebMvc/Models/Enums/SaleStatus.cs
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -5,7 +5,7 @@ namespace SalesWebMvc.Models.Enums
     public enum SaleStatus : int
     {
         Pending = 0, //pendente
-        Billet = 1, // faturado
+        Billed = 1, // faturado
         Canceled = 2 // cancelado
 
 
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -39,14 +39,17 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                       builder.MigrationsAssembly("SalesWebMvc")));
+            services.AddScoped<SeendingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeendingService seendingService)//acrescentei aqui
+            // o SeedingService será automatico pois já for enjetado uma dependencia acima
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seendingService.Seed();// se estiver no perfil de desenvolvimento e a base não tiver populada 
             }
             else
             {
