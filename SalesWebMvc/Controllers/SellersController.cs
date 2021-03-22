eb70436e65acb93ad962f6679ce780e027c9f961
Daniel Castro <d.13castrosilva@gmail.com>
2021-03-22 11:06:45 -0300

    SellerService and basic FinAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,21 @@ using System.Threading.Tasks;
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
-    {
+    {//1º criar uma dependencia para sellerservice
+
+        private readonly SellerService _sellerService;
+
+        //construtor para injetar dependecia
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //esta operação vai retornar uma lista de seller
+            var list = _sellerService.FinAll(); 
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    // após criar a classe tenho que declarar ela no servico de dependencia que fica lá no startup
+    public class SellerService
+    {
+        //gerando dependencia para dbContext
+        //usando readonly boa pratica para previnir que essa dependecia não possa ser alterada
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        // operação findall para retornar uma lista com todos os vendedores
+        public List<Seller> FinAll()
+        {
+            return _context.Sellers.ToList();
+        }
+    }
+}
